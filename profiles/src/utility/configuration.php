f415e8d24e2f8a2ce859d15c26d256b6dcfd1307
Hossain Mamun <hossain-mamun@users.noreply.github.com>
2016-02-18 17:49:47 +0600

    Update configuration.php

--- profiles/src/utility/configuration.php
+++ profiles/src/utility/configuration.php
@@ -7,7 +7,7 @@ class Configuration{
 	const HOSTNAME    = 'localhost';
 	const USER        = 'root';
 	const DBNAME      = 'code';
-	const PASSWORD    = 'phpdeveloper';
+	const PASSWORD    = '';
 
 	const UPLOAD_DIR  = DIRECTORY_SEPARATOR."FinalProject".DIRECTORY_SEPARATOR.'profiles'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR."profile_pic".DIRECTORY_SEPARATOR;
 	const WEBROOT     = 'http://localhost/FinalProject/';
